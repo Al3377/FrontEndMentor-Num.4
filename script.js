0c2f7b03edfde6ada9e3600eda6e31d3372f27d2
Al3377 <alen.vitas@yahoo.com>
2023-03-22 21:16:10 +0100

    working on js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const cardName = document.getElementById("card-name");
 
 const numberInput = document.getElementById("number");
 const cardNumber = document.getElementById("card-number");
+const numberError = document.getElementById("number-error");
 
 const monthInput = document.getElementById("date-month");
 const cardMonth = document.getElementById("card-month");
@@ -18,8 +19,16 @@ nameInput.addEventListener("input", () => {
 });
 
 numberInput.addEventListener("input", () => {
-  cardNumber.textContent = numberInput.value;
+  if (isNaN(numberInput.value)) {
+    numberInput.classList.add("error");
+    numberError.classList.add("error");
+  } else {
+    cardNumber.textContent = numberInput.value;
+    numberInput.classList.remove("error");
+    numberError.classList.remove("error");
+  }
 });
+
 monthInput.addEventListener("input", () => {
   cardMonth.textContent = monthInput.value;
 });
@@ -31,3 +40,17 @@ yearInput.addEventListener("input", () => {
 cvcInput.addEventListener("input", () => {
   cardCvc.textContent = cvcInput.value;
 });
+
+function addSpaces(element) {
+  // Get the current value of the input field
+  var value = element.value;
+
+  // Remove all non-numeric characters
+  value = value.replace(/\D/g, "");
+
+  // Add a space after every 4 digits
+  value = value.replace(/(\d{4})/g, "$1 ");
+
+  // Update the value of the input field
+  element.value = value;
+}
