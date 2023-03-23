f5ea446e971eac23e1eadc09f68630634affc41d
Al3377 <alen.vitas@yahoo.com>
2023-03-23 21:21:50 +0100

    working with active-states

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// select input elements and their corresponding card elements
 const nameInput = document.getElementById("name");
 const cardName = document.getElementById("card-name");
 
@@ -14,43 +15,50 @@ const cardYear = document.getElementById("card-year");
 const cvcInput = document.getElementById("cvc");
 const cardCvc = document.getElementById("card-cvc");
 
+// add event listener to update card name with name input value
 nameInput.addEventListener("input", () => {
   cardName.textContent = nameInput.value;
 });
 
+// add event listener to validate number input and update card number
+const allButNumber = /[^0-9\s]/g;
 numberInput.addEventListener("input", () => {
-  if (isNaN(numberInput.value)) {
+  if (allButNumber.test(numberInput.value)) {
+    // if input contains non-numeric and non-space characters, add error class to input and error message element
     numberInput.classList.add("error");
-    numberError.classList.add("error");
+    numberError.classList.add("error-text");
   } else {
+    // if input only contains numeric and space characters, update card number and remove error class from input and error message element
+    cardNumber.textContent = numberInput.value;
     cardNumber.textContent = numberInput.value;
     numberInput.classList.remove("error");
-    numberError.classList.remove("error");
+    numberError.classList.remove("error-text");
   }
 });
 
+// add event listener to update card month with month input value
 monthInput.addEventListener("input", () => {
   cardMonth.textContent = monthInput.value;
 });
 
+// add event listener to update card year with year input value
 yearInput.addEventListener("input", () => {
   cardYear.textContent = yearInput.value;
 });
 
+// add event listener to update card CVC with CVC input value
 cvcInput.addEventListener("input", () => {
   cardCvc.textContent = cvcInput.value;
 });
 
-function addSpaces(element) {
-  // Get the current value of the input field
-  var value = element.value;
+// add event listener to format number input with spaces after every 4 characters
+numberInput.addEventListener("input", (event) => {
+  const value = event.target.value; // get the input value
+  const formattedValue = value.replace(/\s/g, "").replace(/(.{4})/g, "$1 "); // format the value by adding space after every 4 characters
+  event.target.value = formattedValue.trim(); // update the input value with the formatted value
+});
 
-  // Remove all non-numeric characters
-  value = value.replace(/\D/g, "");
+// selecting button element
+const button = document.getElementById("btn");
 
-  // Add a space after every 4 digits
-  value = value.replace(/(\d{4})/g, "$1 ");
-
-  // Update the value of the input field
-  element.value = value;
-}
+// adding event listener for button to
